4ed8be28b3e6f627500e0792718f437d8e35b19b
Ross Lodge <ross.lodge@electronicmuse.com>
2026-08-16 17:25:29 -0700

    Add CSP

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,38 @@
 import { updateSession } from "@/lib/supabase/middleware";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+
+function buildCsp(nonce: string) {
+  return `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' https://*.supabase.co blob: data:;
+    font-src 'self';
+    connect-src 'self' https://*.supabase.co wss://*.supabase.co;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+  `
+    .replace(/\s{2,}/g, " ")
+    .trim();
+}
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const csp = buildCsp(nonce);
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set("Content-Security-Policy", csp);
+
+  const requestWithCsp = new NextRequest(request, { headers: requestHeaders });
+
+  const response = await updateSession(requestWithCsp);
+  response.headers.set("Content-Security-Policy", csp);
+
+  return response;
 }
 
 export const config = {
